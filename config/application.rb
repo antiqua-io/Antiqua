afcf9ff9f15d9024d00a8070dab6fe384023d504
Ryan Cook <cookrn@gmail.com>
2012-02-19 13:27:30 -0700

    autoload the correct paths

--- config/application.rb
+++ config/application.rb
@@ -28,7 +28,11 @@ module Antiqua
     # -- all .rb files in that directory are automatically loaded.
 
     # Custom directories with classes and modules you want to be autoloadable.
-    config.autoload_paths += %W( #{config.root}/app/workers )
+    config.autoload_paths += %W(
+      #{config.root}/app/collections
+      #{config.root}/app/presenters
+      #{config.root}/app/workers
+    )
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
