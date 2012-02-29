d654b78b5d34ccf54f395da12f0e2ad6a6bb4469
Ryan Cook <cookrn@gmail.com>
2012-02-29 12:12:48 -0700

    load our shell environment via ~/.profile if it exists

--- exe/archive_upload_worker.bash
+++ exe/archive_upload_worker.bash
@@ -1,2 +1,6 @@
 #!/usr/bin/env bash
+if [ -e "~/.profile" ]
+then
+ source ~/.profile
+fi
 /usr/bin/env rvm-shell "$APP_RUBY" -c "bundle exec rake resque:work QUEUE="$APP_ENV"_archive_upload"
--- exe/web.bash
+++ exe/web.bash
@@ -1,2 +1,6 @@
 #!/usr/bin/env bash
+if [ -e "~/.profile" ]
+then
+ source ~/.profile
+fi
 /usr/bin/env rvm-shell "$APP_RUBY" -c "bundle exec rails s thin -p $PORT -e $RACK_ENV"
