5bd3dcd11e0871ba48cabde5d09cdea141de51d8
Ryan Cook <cookrn@gmail.com>
2012-02-29 10:50:56 -0700

    adjust asset precompile strategy to use top-level manifest for subpage specific js

--- config/environments/demo.rb
+++ config/environments/demo.rb
@@ -15,10 +15,13 @@ Antiqua::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Generate digests for assets URLs
   config.assets.digest = true
+
+  # Defaults to Rails.root.join("public/assets")
+  config.assets.manifest = Rails.root.join "public/assets"
 
   # Specifies the header that your server uses for sending files
   config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
@@ -34,5 +37,5 @@ Antiqua::Application.configure do
   config.active_support.deprecation = :notify
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( antiqua/repositories.js modernizr/2.0.6.js )
+  config.assets.precompile += %w( repositories.js modernizr/2.0.6.js )
 end
