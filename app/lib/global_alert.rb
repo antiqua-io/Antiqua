71af53f623abc50174d1fad782131f67c7f53b43
Ryan Cook <cookrn@gmail.com>
2012-03-01 13:39:53 -0700

    update global alert for demo users

--- app/lib/global_alert.rb
+++ app/lib/global_alert.rb
@@ -1,6 +1,6 @@
 module GlobalAlert
   ALERTS = Map.new( {
-    "demo_warning" => "<strong>Warning!</strong> This is just a demo. Do NOT depend on any archives at this time."
+    "demo_warning" => "<strong>Warning!</strong> This is just a demo. Do NOT depend on any archives at this time. You can make as many archives as you want and you will NOT be charged."
   } )
 
   def []( id )
