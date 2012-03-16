2a5ea5d3742f40a94e9bd969acadb53c079a2446
Ryan Cook <cookrn@gmail.com>
2012-03-15 19:06:27 -0600

    support multiple failure backends in Resque

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,2 +1,14 @@
+require 'resque/failure/multiple'
+require 'resque/failure/airbrake'
+require 'resque/failure/redis'
+
 resque_config = YAML.load_file "#{ Rails.root }/config/resque.yml"
-Resque.redis = resque_config[ Rails.env ] || resque_config[ "default" ]
+Resque.redis  = resque_config[ Rails.env ] || resque_config[ "default" ]
+
+Resque::Failure::Airbrake.configure do | config |
+  config.api_key = CONFIG.airbrake_api_key
+  config.secure  = true
+end
+
+Resque::Failure::Multiple.classes = [ Resque::Failure::Redis , Resque::Failure::Airbrake ]
+Resque::Failure.backend           = Resque::Failure::Multiple
