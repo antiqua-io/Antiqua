13f9a2e13815c751732e17ab1b3c938e67aa4423
Ryan Cook <cookrn@gmail.com>
2012-03-15 20:53:16 -0600

    make sure we're forcing SSL in production (should fix insecure session cookies)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,7 +25,7 @@ Antiqua::Application.configure do
   config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true
 
   # See everything in the log (default is :info)
   # config.log_level = :debug
@@ -41,9 +41,6 @@ Antiqua::Application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
-
-  # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( search.js )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
